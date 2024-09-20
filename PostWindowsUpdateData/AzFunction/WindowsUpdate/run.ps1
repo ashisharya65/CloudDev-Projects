0a1a75452117a12c87b820816156538aaec37000
Ashish Arya <56192979+ashisharya65@users.noreply.github.com>
2024-09-20 11:14:38 +0530

    Create run.ps1

---
+++ PostWindowsUpdateData/AzFunction/WindowsUpdate/run.ps1
@@ -0,0 +1,23 @@
+param($Request, $TriggerMetadata)
+
+# Read the incoming JSON data from the HTTP request body
+$requestBody = Get-Content -Raw -Path $Request.Body 
+$UpdateData = $requestBody | ConvertFrom-Json
+
+# Process each update record from the request body
+$CosmosOut = @()
+foreach ($Update in $UpdateData) {
+    $updateRecord = [PSCustomObject]@{
+        deviceid      = $Update.deviceid
+        description   = $Update.description
+        kbArticle     = $Update.kbArticle
+        installedBy   = $Update.installedBy
+        installedDate = $Update.installedDate
+    }
+
+    # Add the update record to the CosmosDB output
+    $CosmosOut += $updateRecord
+}
+
+# Use Push-OutputBinding to send the data to CosmosDB
+Push-OutputBinding -Name CosmosOut -Value $CosmosOut
